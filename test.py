dec3137b899fa108ab7ccd3e4cc6331ff5833c7f
ihanseol <hanseol33@naver.com>
2019-05-10 10:40:16 +0900

    something changed

--- test.py
+++ test.py
@@ -4,9 +4,14 @@ from os import rename
 
 import requests
 
-name = input("input your name : ")
-print("Hello, ", name)
+//name = input("input your name : ")
+//print("Hello, ", name)
 
 for i in range(10):
     print("num : {}".format(i))
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+print(r.ok)
+
 
